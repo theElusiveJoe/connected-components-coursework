0a066b233ab0537086a4269e67968e7c9c4f1109
f <lol@gmail.com>
2023-04-14 12:55:39 +0300

    эти штуки тоже надо коммитить

--- main.go
+++ main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"connectedComponents/prefixTree"
+	"connectedComponents/fastSV"
 )
 
 func main() {
-	prefixTree.TestMap("someData/500k.csv")
-	// display_stats()
-	// fmt.Println(len(records), "nodes")
-	// fmt.Println(unsafe.Sizeof(root.children))
+	fastSV.TestAll("tests/graphs/")
 }
